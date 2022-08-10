ce15fe31cadf59ca635551c6d63b0c3284e1c825
Leonardo Andrade <leonardo.lopes.andrade@gmail.com>
2022-08-10 15:57:10 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Conversor de temperatura de Fahrenheit para Celsius e vice-versa desenvolvido com base no vídeo do canal Feature Code (https://youtu.be/EByVixN_St4).
+
+O design original CSS foi alterado criando um campo dinâmico por meio do DOM para apresentar o resultado da conversão
---
+++ _assets/_script/script.js
@@ -0,0 +1,37 @@
+const qS = (el) => document.querySelector(el);
+const qSAll = (el) => document.querySelectorAll(el);
+
+const grauF = qS("#fahrenheit");
+const grauC = qS("#celsius");
+
+const buttonConvert = qS("#convert");
+const buttonReset = qS("#reset");
+
+const convert = () => {
+  let f = grauF.value;
+  let c = grauC.value;
+  if (f != "") {
+    grauC.value = "";
+    c = (f - 32) / 1.8;
+    qS(".result h2").textContent = `${f} ºF é igual a ${c.toFixed(1)} ºC`;
+    qS(".result").style.display = "flex";
+  } else if (c != "") {
+    grauF.value = "";
+    f = "";
+    f = 1.8 * c + 32;
+    qS(".result h2").textContent = `${c} ºC é igual a ${f.toFixed(1)} ºF`;
+    qS(".result").style.display = "flex";
+  }
+  grauC.value = "";
+  grauF.value = "";
+};
+
+const reset = () => {
+  grauF.value = "";
+  grauC.value = "";
+  qS(".result").style.display = "none";
+};
+
+buttonConvert.addEventListener("click", convert);
+
+buttonReset.addEventListener("click", reset);
